0248f8184a5c1529605d29c35a4414430f2160d5
mdivani <georgemdivani.gm@gmail.com>
2018-05-27 16:48:46 +0400

    shapeshift api added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.css';
 import './styles/main.css';
 
 ReactDOM.render(<Header />, document.getElementById('root'));
